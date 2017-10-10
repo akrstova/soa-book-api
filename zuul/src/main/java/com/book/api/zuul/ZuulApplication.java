242bb73dfcb0206f948dd149fd3e0bc1bfb45cec
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-10-10 23:06:32 +0200

    - updated zuul with new config

--- zuul/src/main/java/com/book/api/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/book/api/zuul/ZuulApplication.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class ZuulApplication {
 
     public static void main(String[] args) {

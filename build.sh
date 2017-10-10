5de649ab27da4685947fa9cc0ce4526a2ce1e883
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-10-10 21:35:05 +0200

    - added build script

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd api-gateway
+mvn package docker:build
+cd ..
+
+cd author
+mvn package docker:build
+cd ..
+
+cd book
+mvn package docker:build
+cd ..
+
+cd eureka
+mvn package docker:build
+cd ..
+
+cd random-search
+mvn package docker:build
+cd ..
+
+cd rating
+mvn package docker:build
+cd ..
+
+cd user
+mvn package docker:build
+cd ..
+
+cd user-preferences
+mvn package docker:build
+cd ..
+
+cd zuul
+mvn package docker:build
+cd ..

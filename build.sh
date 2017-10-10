81bb6cfc3c8a49a40615acb8b3cd5b5e4321a190
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2017-10-10 22:22:23 +0200

    - updated build script to skip tests

--- build.sh
+++ build.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 cd api-gateway
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd author
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd book
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd eureka
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd genre
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd random-search
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd rating
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd user
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd user-preferences
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
 
 cd zuul
-mvn package docker:build
+mvn package docker:build -DskipTests
 cd ..
